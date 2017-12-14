95d83f9bc41e22329c89d145aa616020e44dfc65
Garin YR <ramadika1692@gmail.com>
2017-12-14 22:15:30 +0700

    update v 1.0.0

---
+++ project02/js/main.js
@@ -0,0 +1,160 @@
+$(document).ready(function() {
+	// Header Scroll
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 60) {
+			$('#myPage').addClass('fixed');
+		} else {
+			$('#myPage').removeClass('fixed');
+		}
+	});
+
+	// Fancybox
+	$('.work-box').fancybox();
+
+	// Flexslider
+	$('.flexslider').flexslider({
+		animation: "fade",
+		directionNav: false,
+	});
+
+	// Page Scroll
+	var sections = $('section')
+		nav = $('nav[role="navigation"]');
+
+	$(window).on('scroll', function () {
+	  	var cur_pos = $(this).scrollTop();
+	  	sections.each(function() {
+	    	var top = $(this).offset().top - 76
+	        	bottom = top + $(this).outerHeight();
+	    	if (cur_pos >= top && cur_pos <= bottom) {
+	      		nav.find('a').removeClass('active');
+	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    	}
+	  	});
+	});
+	nav.find('a').on('click', function () {
+	  	var $el = $(this)
+	    	id = $el.attr('href');
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - 75
+		}, 500);
+	  return false;
+	});
+
+	// Mobile Navigation
+	$('.nav-toggle').on('click', function() {
+		$(this).toggleClass('close-nav');
+		nav.toggleClass('open');
+		return false;
+	});	
+	nav.find('a').on('click', function() {
+		$('.nav-toggle').toggleClass('close-nav');
+		nav.toggleClass('open');
+	});
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+
+
+// Carousel Auto-Cycle
+  $(document).ready(function() {
+    $('.carousel2').carousel2({
+      interval: 6000
+    })
+  });
+
+
+
+//header carousel
+  [2:00 AM, 12/2/2017] SI A Hotib: $(document).ready(function()
+{
+	
+	// Closes the sidebar menu on menu-close button click event
+	$("#menu-close").click(function(e)							//declare the element event ...'(e)' = event (shorthand)
+	{
+																// - will not work otherwise")
+		$("#sidebar-wrapper").toggleClass("active");			//instead on click event toggle active CSS element
+		e.preventDefault(); 									//prevent the default action ("Do not remove as the code
+		
+		/*!
+		======================= Notes ===============================
+		* see: .sidebar-wrapper.active in: style.css
+		==================== END Notes ==============================
+		*/
+	});															//Close 'function()'
+
+	// Open the Sidebar-wrapper on Hover
+	$("#menu-toggle").hover(function(e)							//declare the element event ...'(e)' = event (shorthand)
+	{
+		$("#sidebar-wrapper").toggleClass("active",true);		//instead on click event toggle active CSS element
+		e.preventDefault();										//prevent the default action ("Do not remove as the code
+	});
+
+	$("#menu-toggle").bind('click',function(e)					//declare the element event ...'(e)' = event (shorthand)
+	{
+		$("#sidebar-wrapper").toggleClass("active",true);		//instead on click event toggle active CSS element
+		e.preventDefault();										//prevent the default action ("Do not remove as the code
+	});															//Close 'function()'
+
+	$('#sidebar-wrapper').mouseleave(function(e)				//declare the jQuery: mouseleave() event 
+																// - see: ('//api.jquery.com/mouseleave/' for details)
+	{
+		/*! .toggleClass( className, state ) */
+		$('#sidebar-wrapper').toggleClass('active',false);		/* toggleClass: Add or remove one or more classes from each element
+																in the set of matched elements, depending on either the class's
+																presence or the value of the state argument */
+		e.stopPropagation();									//Prevents the event from bubbling up the DOM tree
+																// - see: ('//api.jquery.com/event.stopPropagation/' for details)
+																
+		e.preventDefault();										// Prevent the default action of the event will not be triggered
+																// - see: ('//api.jquery.com/event.preventDefault/' for details)
+	});
+});
+// Closes the sidebar menu on menu-close button click event
+$("#menu-close").click(function(e)							//declare the element event ...'(e)' = event (shorthand)
+{
+															// - will not work otherwise")
+	$("#sidebar-wrapper").toggleClass("active");			//instead on click event toggle active CSS element
+	e.preventDefault(); 									//prevent the default action ("Do not remove as the code
+	
+	/*!
+	======================= Notes ===============================
+	* see: .sidebar-wrapper.active in: style.css
+	==================== END Notes ==============================
+	*/
+});
